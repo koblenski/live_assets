c06a07cb89e2b33a845018a8dece16a13a7526da
Sam Koblenski <koblenski@gmail.com>
2014-03-15 20:43:34 -0500

    add threading and a message queue for CSS change notifications

--- lib/live_assets.rb
+++ lib/live_assets.rb
@@ -1,4 +1,45 @@
 require "live_assets/engine"
+require "thread"
+require "listen"
 
 module LiveAssets
+  autoload :SSESubscriber, "live_assets/sse_subscriber"
+  extend ActiveSupport::Autoload
+
+  eager_autoload do
+    autoload :SSESubscriber
+  end
+
+  mattr_reader :subscribers
+  @@subscribers = []
+  @@mutex = Mutex.new
+
+  def self.subscribe(subscriber)
+    @@mutex.synchronize do
+      subscribers << subscriber
+    end
+  end
+
+  def self.unsubscribe(subscriber)
+    @@mutex.synchronize do
+      subscribers.delete(subscriber)
+    end
+  end
+
+  def self.start_listener(event, directories)
+    Thread.new do
+      Listen.to(*directories, latency: 0.5) do |_modified, _added, _removed|
+        subscribers.each { |s| s << event }
+      end.start
+    end
+  end
+
+  def self.start_timer(event, time)
+    Thread.new do
+      while true
+        subscribers.each { |s| s << event }
+        sleep(time)
+      end
+    end
+  end
 end
--- test/live_assets_test.rb
+++ test/live_assets_test.rb
@@ -1,7 +1,62 @@
 require 'test_helper'
+require 'fileutils'
 
 class LiveAssetsTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, LiveAssets
+  end
+
+  setup do
+    FileUtils.mkdir_p "test/tmp"
+  end
+
+  teardown do
+    FileUtils.rm_rf "test/tmp"
+  end
+
+  test "can subscribe to listener events" do
+    l = LiveAssets.start_listener(:reload, ["test/tmp"])
+    subscriber = []
+    LiveAssets.subscribe(subscriber)
+
+    begin
+      while subscriber.empty?
+        File.write("test/tmp/sample", SecureRandom.hex(20))
+      end
+
+      assert_includes subscriber, :reload
+    ensure
+      LiveAssets.unsubscribe(subscriber)
+      l.kill
+    end
+  end
+
+  test "can subscribe to existing reloadCSS events" do
+    subscriber = []
+    LiveAssets.subscribe(subscriber)
+
+    begin
+      while subscriber.empty?
+        FileUtils.touch("test/dummy/app/assets/stylesheets/application.css")
+      end
+
+      assert_includes subscriber, :reloadCSS
+    ensure
+      LiveAssets.unsubscribe(subscriber)
+    end
+  end
+
+  test "receives timer notifications" do
+    l = LiveAssets.start_timer(:ping, 0.5)
+
+    subscriber = []
+    LiveAssets.subscribe(subscriber)
+
+    begin
+      true while subscriber.empty?
+      assert_includes subscriber, :ping
+    ensure
+      LiveAssets.unsubscribe(subscriber)
+    end
   end
 end
